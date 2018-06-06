0e472f249d2fe517fb71d6069368f85950f89f37
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-06 15:58:22 +0100

    artist.albums method added

--- db/console.rb
+++ db/console.rb
@@ -2,20 +2,42 @@ require('pry')
 require_relative('../models/album.rb')
 require_relative('../models/artist.rb')
 
-
+Album.delete_all()
+Artist.delete_all()
 
 
 
 artist1 = Artist.new({'name' => "Abba"})
 artist1.save()
 
-
 album1 = Album.new({
   'title' => 'Arrival', 'artist_id' => artist1.id()
   })
+album1.save()
+
+album2 = Album.new({
+  'title' => 'Waterloo', 'artist_id' => artist1.id()
+  })
+album2.save()
+
+album3 = Album.new({
+  'title' => 'Voulez-Vous', 'artist_id' => artist1.id()
+  })
+album3.save()
 
 
+artist2 = Artist.new({'name' => "Bros"})
+artist2.save()
 
+album4 = Album.new({
+  'title' => 'Push', 'artist_id' => artist2.id()
+  })
+album4.save()
+
+album5 = Album.new({
+  'title' => 'The Time', 'artist_id' => artist2.id()
+  })
+album5.save()
 
 
 
