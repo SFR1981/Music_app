7829fd50cdaf532fe6221d828a0c9c5614866552
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-06 15:20:34 +0100

    artist.rb created

---
+++ models/artist.rb
@@ -0,0 +1,16 @@
+require('pg')
+require_relative('../db/sql_runner.rb')
+
+class Artist
+
+  attr_accessor :name
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+  
+
+end
