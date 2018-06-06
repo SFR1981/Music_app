0c5d077356aad3aedaa372791558e899b5aab894
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-06-06 15:13:39 +0100

    music_collection.sql set up

---
+++ db/music_collection.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+  id SERIAL2 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE albums (
+  id SERIAL2 PRIMARY KEY,
+  name VARCHAR(255),
+  artist_id INT2 REFERENCING artists(id)
+);
